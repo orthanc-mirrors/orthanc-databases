eec663c3ce06356d898ea2123d8752e83819cbb3
Alain Mazy <alain@mazy.be>
2020-07-13 11:50:54 +0200

    reduced verbosity

--- Framework/MySQL/MySQLStatement.cpp
+++ Framework/MySQL/MySQLStatement.cpp
@@ -355,7 +355,7 @@ namespace OrthancDatabases
       throw Orthanc::OrthancException(Orthanc::ErrorCode_Database);
     }
 
-    LOG(INFO) << "Preparing MySQL statement: " << sql;
+    VLOG(1) << "Preparing MySQL statement: " << sql;
 
     db_.CheckErrorCode(mysql_stmt_prepare(statement_, sql.c_str(), sql.size()));
 
