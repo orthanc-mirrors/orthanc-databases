c5d3cc648e968b85cfb149c9f84100549251103b
Sebastien Jodogne <s.jodogne@gmail.com>
2021-01-28 16:54:51 +0100

    CMake option "ORTHANC_SDK_VERSION" can be set to "framework" for developers

--- Resources/CMake/DatabasesPluginParameters.cmake
+++ Resources/CMake/DatabasesPluginParameters.cmake
@@ -25,7 +25,7 @@ set(ORTHANC_FRAMEWORK_ROOT "" CACHE STRING "Path to the Orthanc source directory
 
 # Advanced parameters to fine-tune linking against system libraries
 set(USE_SYSTEM_ORTHANC_SDK ON CACHE BOOL "Use the system version of the Orthanc plugin SDK")
-set(ORTHANC_SDK_VERSION "1.5.4" CACHE STRING "Version of the Orthanc plugin SDK to use, if not using the system version (can be \"0.9.5\", \"1.4.0\", \"1.5.2\", or \"1.5.4\")")
+set(ORTHANC_SDK_VERSION "1.5.4" CACHE STRING "Version of the Orthanc plugin SDK to use, if not using the system version (can be \"0.9.5\", \"1.4.0\", \"1.5.2\", \"1.5.4\", or \"framework\")")
 
 include(${CMAKE_CURRENT_LIST_DIR}/DatabasesFrameworkParameters.cmake)
 
