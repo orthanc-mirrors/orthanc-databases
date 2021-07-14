e5fd4c7b0299a4c3bdd5d32b2cc8c4bd7a664b74
Sebastien Jodogne <s.jodogne@gmail.com>
2021-07-14 18:29:27 +0200

    speed up compilation by sharing library between plugins

--- Framework/Plugins/IndexUnitTests.h
+++ Framework/Plugins/IndexUnitTests.h
@@ -676,8 +676,6 @@ TEST(IndexBackend, Basic)
       a.compressedHash = "md5";
       db.AddAttachment(*manager, resources[attachmentLevel], a, 42);
 
-      ASSERT_THROW(db.AddAttachment(*manager, resources[attachmentLevel], a, 43), Orthanc::OrthancException);
-      
       deletedAttachments.clear();
       a.uuid = "attachment2";
       db.DeleteAttachment(*output, *manager, resources[attachmentLevel], Orthanc::FileContentType_DicomAsJson);
