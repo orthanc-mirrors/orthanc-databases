80cb6e0a2ad789b237ca3bb56cffe15db6b9be34
Alain Mazy <am@orthanc.team>
2024-09-25 09:25:58 +0200

    added missing files
    --HG--
    branch : attach-custom-data

---
+++ PostgreSQL/Plugins/SQL/Upgrades/Rev2ToRev3.sql
@@ -0,0 +1,19 @@
+-- This file contains part of the changes required to upgrade from Revision 2 to Revision 3 (DB version 6)
+-- It actually contains only the changes that:
+   -- can not be executed with an idempotent statement in SQL
+   -- or would polute the PrepareIndex.sql
+-- This file is executed only if the current schema is in revision 2 and it is executed 
+-- before PrepareIndex.sql that is idempotent.
+
+
+
+DO $body$
+BEGIN
+
+    BEGIN
+        ALTER TABLE AttachedFiles ADD COLUMN customData TEXT;
+    EXCEPTION
+        WHEN duplicate_column THEN RAISE NOTICE 'column customData already exists in AttachedFiles.';
+    END;
+
+END $body$ LANGUAGE plpgsql;
