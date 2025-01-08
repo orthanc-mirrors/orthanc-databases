e12c962f09f25ac7fd21d89d1ca48d330d552fa7
Alain Mazy <am@orthanc.team>
2025-01-08 15:18:33 +0100

    tools/find: fix query by ModalitiesInStudy with pagination

--- Framework/Plugins/ISqlLookupFormatter.cpp
+++ Framework/Plugins/ISqlLookupFormatter.cpp
@@ -1023,7 +1023,25 @@ namespace OrthancDatabases
       {
         std::string join;
         FormatJoin(join, constraint, count);
-        joins += join;
+
+
+        if (constraint.GetLevel() <= queryLevel)
+        {
+          joins += join;
+        }
+        else if (constraint.GetLevel() == queryLevel + 1 && !comparison.empty())
+        {
+          // new in v 1.12.6, the constraints on child tags are actually looking for one child with this value
+          comparison = " EXISTS (SELECT 1 FROM Resources AS " + FormatLevel(static_cast<Orthanc::ResourceType>(queryLevel + 1)) + 
+                       join + 
+                       " WHERE " + comparison + " AND " + 
+                       FormatLevel(static_cast<Orthanc::ResourceType>(queryLevel + 1)) + ".parentId = " + FormatLevel(static_cast<Orthanc::ResourceType>(queryLevel)) + ".internalId) ";
+        }
+
+        if (!comparison.empty())
+        {
+          comparisons += " AND " + comparison;
+        }
 
         if (!comparison.empty())
         {
@@ -1062,13 +1080,14 @@ namespace OrthancDatabases
               FormatLevel(static_cast<Orthanc::ResourceType>(level + 1)) + ".parentId");
     }
 
-    for (int level = queryLevel + 1; level <= lowerLevel; level++)
-    {
-      sql += (" INNER JOIN Resources " +
-              FormatLevel(static_cast<Orthanc::ResourceType>(level)) + " ON " +
-              FormatLevel(static_cast<Orthanc::ResourceType>(level - 1)) + ".internalId=" +
-              FormatLevel(static_cast<Orthanc::ResourceType>(level)) + ".parentId");
-    }
+    // disabled in v 1.12.6 now that the child levels are considered as "is there at least one child that meets this constraint"
+    // for (int level = queryLevel + 1; level <= lowerLevel; level++)
+    // {
+    //   sql += (" INNER JOIN Resources " +
+    //           FormatLevel(static_cast<Orthanc::ResourceType>(level)) + " ON " +
+    //           FormatLevel(static_cast<Orthanc::ResourceType>(level - 1)) + ".internalId=" +
+    //           FormatLevel(static_cast<Orthanc::ResourceType>(level)) + ".parentId");
+    // }
 
     std::list<std::string> where;
     where.push_back(strQueryLevel + ".resourceType = " +
