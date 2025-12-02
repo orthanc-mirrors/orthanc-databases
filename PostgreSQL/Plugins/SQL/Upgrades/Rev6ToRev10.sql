d81ad43fe40a3a46e2f16686cb3469bc9645e11e
Alain Mazy <am@orthanc.team>
2025-12-02 15:44:14 +0100

    fix upgrade

--- PostgreSQL/Plugins/SQL/Upgrades/Rev6ToRev10.sql
+++ PostgreSQL/Plugins/SQL/Upgrades/Rev6ToRev10.sql
@@ -1,7 +1,43 @@
 -- Adding a PK to these 2 table to allow pg_repack to process these tables, enabling reclaiming disk space and defragmenting the tables.
 
-ALTER TABLE InvalidChildCounts ADD COLUMN pk BIGSERIAL PRIMARY KEY;
-ALTER TABLE GlobalIntegersChanges ADD COLUMN pk BIGSERIAL PRIMARY KEY;
+-- in rev 10, we need to modify the table InvalidChildCounts that is created in rev 4 but,
+-- the table is actually created in PrepareDatabase.sql that will be run after the upgrade script
+-- hence this check before executin the ALTER TABLE.
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+        AND table_name = 'invalidchildcounts'
+    ) THEN
+        ALTER TABLE InvalidChildCounts ADD COLUMN pk BIGSERIAL PRIMARY KEY;
+    END IF;
+END $$;
 
--- Adding the queues timeout
-ALTER TABLE Queues ADD COLUMN reservedUntil BIGINT DEFAULT NULL;
+-- same for GlobalIntegersChanges table that is created in rev2:
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+        AND table_name = 'globalintegerschanges'
+    ) THEN
+        ALTER TABLE GlobalIntegersChanges ADD COLUMN pk BIGSERIAL PRIMARY KEY;
+    END IF;
+END $$;
+
+
+-- Adding the queues timeout (Queues table was created in rev5)
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+        AND table_name = 'queues'
+    ) THEN
+        ALTER TABLE Queues ADD COLUMN reservedUntil BIGINT DEFAULT NULL;
+    END IF;
+END $$;
